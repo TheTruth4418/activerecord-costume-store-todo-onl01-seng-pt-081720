5e9dbc8eaeb7ab9001ee0928042ff84be1bb0063
William West <wwest466@gmail.com>
2020-10-30 04:44:51 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+  def change
+    create :CostumeStore do |x|
+      x.string :name
+      x.string :location
+      x.integer :costume_inventory
+      x.integer :num_of_employees
+      x.boolean :still_in_business
+      x.text :opening_time
+      x.text :closing_time
+    end
+  end
+end
