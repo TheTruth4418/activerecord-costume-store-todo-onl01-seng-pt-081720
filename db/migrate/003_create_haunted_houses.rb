c35cb28d7f1ea0333e69cdc4cda9c594ec89c46c
William West <wwest466@gmail.com>
2020-10-30 04:57:46 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,4 +1,15 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[5.2]
-
+  def change
+    create_table :haunted_house do |x|
+      x.string :name
+      x.string :location
+      x.string :theme 
+      x.integer :price
+      x.boolean :family_friendly
+      x.datetime :opening_time
+      x.datetime :closing_time
+      x.string :description
+    end
+  end
 end
